484a91bbae97e09ada341fc189f856614d4238b2
Dima <dima23790@gmail.com>
2020-02-21 14:00:18 +0200

    Added php code to show all posts

---
+++ blog/includes/_navbar.php
@@ -0,0 +1,16 @@
+<nav class="main-nav">
+    <div class="main-nav__items">
+        <div class="main-nav__logo">
+            <a href="index.php" class="logo">Blogin</a>
+            <span class="dot">.</span>
+        </div>
+        <!-- logo end -->
+        <div class="main-nav__links">
+            <a href="index.php" class="main-nav__link">Journal</a>
+            <a href="#" class="main-nav__link">About</a>
+            <a href="#" class="main-nav__link">Work</a>
+            <a href="#" class="main-nav__link">Contact</a>
+        </div>
+        <!-- /.main-nav__links -->
+    </div>
+</nav>
--- blog/index.php
+++ blog/index.php
@@ -1,48 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require_once 'includes/header.php' ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Document</title>
-</head>
+<?php
+$postObj = new Post();
+$posts = $postObj->getPosts();
+?>
 
-<body>
-    <nav class="main-nav">
-        <div class="main-nav__items">
-            <div class="main-nav__logo">
-                <span class="logo">Blogin</span>
-                <span class="dot">.</span>
-            </div>
-            <!-- logo end -->
-            <div class="main-nav__links">
-                <a href="#" class="main-nav__link">Journal</a>
-                <a href="#" class="main-nav__link">About</a>
-                <a href="#" class="main-nav__link">Work</a>
-                <a href="#" class="main-nav__link">Contact</a>
-            </div>
-            <!-- /.main-nav__links -->
-        </div>
-    </nav>
 
-    <div class="container">
+<div class="container">
+    <?php foreach ($posts as $post) { ?>
         <div class="row">
             <div class="blog-image">
-                <img src="assets/img/4646940.jpg" alt="">
+                <img src="assets/img/<?= $post->image ?>" alt="">
             </div>
             <!-- /.blog-image -->
             <div class="blog-info">
-                <h1 class="blog-info__title">Python Course</h1>
-                <p class="blog-info__description">flasdfjsld;ajvohyoahtradslkhnflkvjadsl;fjsldrlasjsldrlasjsldrlasjsldrlasuflsdajcdsljv</p>
+                <h1 class="blog-info__title"><?= $post->title ?></h1>
+                <p class="blog-info__description"><?= $post->description ?></p>
                 <a href="#" class="blog-info__read-more">Continue Reading</a>
             </div>
             <!-- /.blog-info -->
         </div>
         <!-- /.row -->
-    </div>
-    <!-- /.container -->
-</body>
-
-</html>
+    <?php } ?>
+</div>
+<!-- /.container -->
+<?php require_once 'includes/footer.php' ?>
