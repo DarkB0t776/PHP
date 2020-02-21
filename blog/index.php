fe34cdcc5632497dd453ec4e750ae1a42cc415a7
Dima <dima23790@gmail.com>
2020-02-21 12:51:25 +0200

    Completed navbar index page

--- blog/index.php
+++ blog/index.php
@@ -1,3 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-phpinfo();
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="main-nav">
+        <div class="main-nav__items">
+            <div class="main-nav__logo">
+                <span class="logo">Blogin</span>
+                <span class="dot">.</span>
+            </div>
+            <!-- logo end -->
+            <div class="main-nav__links">
+                <a href="#" class="main-nav__link">Journal</a>
+                <a href="#" class="main-nav__link">About</a>
+                <a href="#" class="main-nav__link">Work</a>
+                <a href="#" class="main-nav__link">Contact</a>
+            </div>
+            <!-- /.main-nav__links -->
+        </div>
+    </nav>
+</body>
+
+</html>
